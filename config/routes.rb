31d16bbaff73c28447009f7c18bb31c72d6637c8
nathan hains <nathanhains14@yahoo.com>
2021-04-22 20:28:19 -0400

    commit tree 1e45324864513f3ead61f2c9fa4bac29ebadf3d3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] 
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/home', to: 'welcome#home'
+
   root 'welcome#home'
 end
